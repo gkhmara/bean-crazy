98f94e6f0d9c62d085b04c084eadc597e5116b92
Greg Khmara <gkhmara@gmail.com>
2022-08-13 23:16:38 -0700

    create form

--- src/components/NewCoffeeForm.js
+++ src/components/NewCoffeeForm.js
@@ -1,9 +1,37 @@
 import React from "react";
+import { v4 } from 'uuid';
 
-function NewCoffeeForm(props) {
+function NewCoffeeForm() {
+
+  function handleNewCoffeeFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.name.value);
+    console.log(event.target.origin.value);
+    console.log(event.target.price.value);
+    console.log(event.target.roast.value);
+  }
+
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleNewCoffeeFormSubmission}>
+      <input
+          type='text'
+          name='name'
+          placeholder='Coffee Name' />
+        <input
+          type='text'
+          name='origin'
+          placeholder='Coffee Origin' />
+        <input
+          type='text'
+          name='price'
+          placeholder='Coffee Price' />
+        <input 
+          type='text'
+          name='roast'
+          placeholder='Coffee Roast'/>
+        <button type='submit'>Submit</button>
+      </form>
     </React.Fragment>
   );
 }
