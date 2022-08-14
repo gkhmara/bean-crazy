a282b45f6cb570ce7437f29ebebf9feeccc6c47d
Greg Khmara <gkhmara@gmail.com>
2022-08-14 02:14:55 -0700

    update to Coffee.js

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -4,20 +4,24 @@ import PropTypes from "prop-types";
 function Coffee(props) {
   return (
     <React.Fragment>
-      <h3>{props.name}</h3>
-      <h3>{props.origin}</h3>
-      <h3>{props.price}</h3>
-      <h3>{props.roast}</h3>
-      <hr/>
+      <div onClick ={() => props.whenCoffeeClicked(props.id)}>
+        <h3>{props.name}</h3>
+        <h3>{props.origin}</h3>
+        <h3>{props.price}</h3>
+        <h3>{props.roast}</h3>
+        <hr/>
+      </div>
     </React.Fragment>
   );
 }
 
 Coffee.propTypes = {
-  name: PropTypes.string.isRequired,
-  origin: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
-  roast: PropTypes.string.isRequired
+  name: PropTypes.string,
+  origin: PropTypes.string,
+  price: PropTypes.string,
+  roast: PropTypes.string,
+  id: PropTypes.string,
+  whenCoffeeClicked: PropTypes.func
 };
 
 export default Coffee;
