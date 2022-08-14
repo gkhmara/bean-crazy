e664c887e61abc00ca0f4e2f31217bb03cb842bf
Greg Khmara <gkhmara@gmail.com>
2022-08-14 10:56:12 -0700

    add delete button

--- src/components/CoffeeDetail.js
+++ src/components/CoffeeDetail.js
@@ -2,20 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function CoffeeDetail(props){
-  const { coffee } = props;
+  const { coffee, onClickingDelete } = props;
 
   return(
     <React.Fragment>
       <h1>Coffee Detail</h1>
       <h3>{coffee.name} - {coffee.price}</h3>
       <p><em>{coffee.origin} - {coffee.roast}</em></p>
+      <button onClick={()=> onClickingDelete(coffee.id) }>Close Ticket</button>
       <hr/>
     </React.Fragment>
   );
 }
 
 CoffeeDetail.propTypes = {
-  coffee: PropTypes.object
+  coffee: PropTypes.object,
+  onClickingDelete: PropTypes.func
 }
 
 export default CoffeeDetail;
