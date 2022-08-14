e293c3539d646ff37a69698a0a6ec8fb78dcde33
Greg Khmara <gkhmara@gmail.com>
2022-08-13 22:27:26 -0700

    update to CoffeeControl.js

--- src/components/CoffeeControl.js
+++ src/components/CoffeeControl.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import NewCoffeeForm from './NewTicketForm';
+import NewCoffeeForm from './NewCoffeeForm';
 import CoffeeList from './CoffeeList';
 
 class CoffeeControl extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      formVisibleOnPage: false
+    };
   }
 
   render(){
+    let currentlyVisibleSate = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleSate = <NewCoffeeForm />
+    } else {
+      currentlyVisibleSate = <CoffeeList />
+    }
     return (
       <React.Fragment>
+        {currentlyVisibleSate}
       </React.Fragment>
     );
   }
