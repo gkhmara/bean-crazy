45fb73015313ba582157e1f5b4c07c90a99e10c6
Greg Khmara <gkhmara@gmail.com>
2022-08-27 21:19:53 -0700

    update to NewCoffeeForm.js

--- src/components/NewCoffeeForm.js
+++ src/components/NewCoffeeForm.js
@@ -12,6 +12,7 @@ function NewCoffeeForm(props) {
       origin: event.target.origin.value,
       price: event.target.price.value,
       roast: event.target.roast.value,
+      quantity: parseInt(130),
       id: v4()
     });
   }
